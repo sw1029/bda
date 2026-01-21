2b1f9fb455bb655cfa00d3ca86805306c51bb136
sw1029 <yunsw62@gmail.com>
2026-01-21 16:08:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+https://dacon.io/competitions/official/236664/overview/description
+
+대회 실험용 레포
+
---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+'''
+모델 관련 모듈로 사용하기 위한 파일
+어지간하면 base.py에 정의된 클래스를 상속하는 방식으로 진행
+가능하면 pytorch lightning과 같이 model.train() 으로 학습 자체도 호출 가능하도록
+config는 config/config.yaml를 hydra로 불러와서 사용
+'''
