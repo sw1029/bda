c3e0b52c15923c9464694e7c3fd9939d0231fbf4
sw1029 <yunsw62@gmail.com>
2026-01-21 20:11:01 +0900

    mlp 구현하다 말음. 지속 구현 예정

---
+++ model/mlp.py
@@ -0,0 +1,16 @@
+from base import model
+import pandas as pd
+from datetime import datetime
+from pathlib import Path
+from omegaconf import OmegaConf
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
+import numpy as np
+
+class mlp(model):
+    def __init__(self):
+        pass
